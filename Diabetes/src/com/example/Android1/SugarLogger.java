ed05e7d1256cbdde969383edbb0060aae0416663
Richard Williams <coldheart042@gmail.com>
2014-02-05 16:12:43 -0800

    Revision 1
    Added new "activity" to app.

---
+++ Diabetes/src/com/example/Android1/SugarLogger.java
@@ -0,0 +1,19 @@
+package com.example.Android1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Richard on 2/5/14.
+ */
+public class SugarLogger extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    initializeApp();
+  }
+  private void initializeApp(){
+
+  }
+}
