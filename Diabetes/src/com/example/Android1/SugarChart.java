ed10860bf8ec74a97cb2c5801f9067369ab072c6
Richard Williams <coldheart042@gmail.com>
2014-02-10 20:43:18 -0800

    Revision 4
    Grid renders correctly, point plotting algorithm just about set up

--- Diabetes/src/com/example/Android1/SugarChart.java
+++ Diabetes/src/com/example/Android1/SugarChart.java
@@ -13,7 +13,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by Richard on 2/5/14.
@@ -64,19 +66,76 @@ public class SugarChart extends Activity {
 
     @Override
     public void onDraw( Canvas canvas){
-      ArrayList<Integer> bsList = new ArrayList<Integer>();
-      // The setup:
-      int originX = 10, originY = 800;
-
       canvas.drawColor(Color.WHITE);
-      canvas.drawLine(originX , originY, 600, originY, paint);
-      canvas.drawLine(originX , originY, originX, originY - 600, paint);
 
-      // The take-down:
+      // Set center coordinates (0,0)
+      int centerX = 40, centerY = canvas.getHeight() - (canvas.getHeight() / 8);
+      drawGrid(canvas, centerX, centerY);
+      ArrayList<Integer> bs = getBS();
+      int size = bs.size();
+      if (size > 0){
+        ArrayList<Integer> placeholder;     // This needs to be set initially as point A
+        int diff = canvas.getWidth() / size;
 
-      for(Integer value : bsList){
-
+        /////////////// Iterates through Blood Sugar (bs) ArrayList \\\\\\\\\\\\\\\
+        for (int i = 0; i < size; i++){
+          // Generate xy of point A (ArrayList<Integer>(x,y))
+          // Generate xy of point B (ArrayList<Integer>(x,y))
+          // Set placeholder to point B
+        }
       }
+    }
+
+    /////////////// This sets up the graph \\\\\\\\\\\\\\\
+    public void drawGrid( Canvas canvas, int centerX, int centerY){
+      Paint axis = new Paint(Color.BLACK);
+      Paint marker = new Paint(Color.BLUE);
+
+      /////////////// Y- Axis \\\\\\\\\\\\\\\
+      canvas.drawLine(centerX, centerY, centerX, centerY - 800, axis); // Draws the Y-Axis
+      canvas.drawLine(centerX, centerY - 800, centerX + 10, centerY - 795, axis); // Y-Axis Arrow (Right)
+      canvas.drawLine(centerX, centerY - 800, centerX - 10, centerY - 795, axis); // Y-Axis Arrow (Left)
+      canvas.drawText("Blood Sugar (Rounded to nearest 5)", centerX, centerY - 810, axis);
+
+      /////////////// X- Axis \\\\\\\\\\\\\\\
+      canvas.drawLine(centerX, centerY, centerX + (canvas.getWidth() - 60),centerY, axis); // Draws the X-Axis
+      canvas.drawLine(centerX + (canvas.getWidth() - 60),centerY,centerX + (canvas.getWidth() - 70), centerY + 10, axis); // Draws the X-Axis Arrow (Bottom)
+      canvas.drawLine(centerX + (canvas.getWidth() - 60),centerY,centerX + (canvas.getWidth() - 70), centerY - 10, axis); // Draws the X-Axis Arrow (Top)
+
+      /////////////// Y-Axis Grid markers \\\\\\\\\\\\\\\
+      for(int i = 20; i < 800; i += 20){
+        String bs = String.valueOf((i / 4));
+        canvas.drawLine(centerX - 5, centerY - i, centerX + 5, centerY - i, marker);
+
+          Paint danger = new Paint(Color.RED);
+          canvas.drawText(bs, centerX - 30, (centerY -i)+5, danger);
+      }
+  }
+
+    /////////////// This sets the array up \\\\\\\\\\\\\\\
+    public ArrayList<Integer> getBS(){
+      ArrayList<Integer> bs = new ArrayList<Integer>();
+      File f = new File("sugar_log.csv");
+      try{
+        FileInputStream fis = new FileInputStream(f);
+        BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+        String line;
+        while ( (line = br.readLine()) != null){
+          String[] rowData = line.split(",");
+          bs.add(Integer.parseInt(rowData[4]));
+        }
+      } catch (Exception e){
+        Toast.makeText(SugarChart.this, "Failed to load file.", Toast.LENGTH_SHORT).show();
+      }
+      return bs;
+    }
+
+    /////////////// This sets up the points and lines on the graph \\\\\\\\\\\\\\\
+    public ArrayList<Integer> plotPoints(Canvas canvas, ArrayList<Integer> pointA, ArrayList<Integer> pointB ){
+      // Draw Circle at pointA
+      // Draw Circle at pointB
+      // Draw Line from pointA to pointB
+      return pointB;
     }
   }
 
