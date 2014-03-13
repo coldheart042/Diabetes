b1479be12896feb0fb2d4af4e838763d435b8021
Richard Williams <coldheart042@gmail.com>
2014-03-13 04:19:29 -0700

    Revert "Revision 8"
    This reverts commit 71ffd5d7067067b45d0f8c8b0fd3da143200d19d.

--- Diabetes/src/com/example/Android1/Alarms.java
+++ Diabetes/src/com/example/Android1/Alarms.java
@@ -1,12 +1,12 @@
 package com.example.Android1;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.*;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -24,6 +24,7 @@ public class Alarms extends Activity {
     inflater.inflate(R.menu.main, menu);
     return true;
   }
+
   @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     switch (item.getItemId()){
@@ -52,7 +53,6 @@ public class Alarms extends Activity {
   }
 
   private void initializeView(){
-    Toast.makeText(Alarms.this, R.string.alarms_load, Toast.LENGTH_LONG);
 
     // Get button and set on-click listener...
     btnAlarm = (Button) findViewById(btnAddAlarm);
@@ -60,55 +60,9 @@ public class Alarms extends Activity {
       @Override
       public void onClick(View view) {
 
-        // Call custom dialog to set alarm
-        dialogBuilder(Alarms.this, 0);
-
       }
-      // Alarm will be referenced as a button, added dynamically - onclick will show second dialog
     });
 
-    // Alarms programming.
-  }
-  private void dialogBuilder(Context context,final int selector){
-    AlertDialog.Builder builder = new AlertDialog.Builder(context);
-    LayoutInflater inflater = getLayoutInflater();
-    builder.setTitle(R.string.add_alarm)
-        .setView(inflater.inflate(R.layout.alarm_dialog, null))
-        .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
-          @Override
-          public void onClick(DialogInterface dialogInterface, int i) {
-            if (selector == 0){
-              // Add Alarm to activity (and save to file) from dialog
-              Toast.makeText(Alarms.this,"Alarm will be created!", Toast.LENGTH_LONG).show();
-            }
-            else {
-              // Get alarm from activity and modify from dialog
-              Toast.makeText(Alarms.this,"Alarm will be saved!", Toast.LENGTH_LONG).show();
-            }
-          }
-        })
-        .setNegativeButton(R.string.back, new DialogInterface.OnClickListener(){
-
-          // Do nothing.
-          @Override
-        public void onClick(DialogInterface dialogInterface, int i){
-            if (selector == 0){
-              Toast.makeText(Alarms.this,"Alarm will not be created!", Toast.LENGTH_LONG).show();
-            }
-            else {
-              Toast.makeText(Alarms.this,"Alarm will not be saved!", Toast.LENGTH_LONG).show();
-            }
-          }
-        });
-    if (selector != 0){
-      builder.setNeutralButton(R.string.delete, new DialogInterface.OnClickListener(){
-        @Override
-        public void onClick(DialogInterface dialogInterface, int i) {
-          // Delete alarm from array
-          // Update the file
-        }
-      });
-    }
-    builder.show();
+    // Add more Stuff here!!!
   }
 }
